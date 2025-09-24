b3710af1c70ab51e4129c38431caaf7ef727faee
W I L L <will@gravina.dev>
2025-09-24 09:30:19 -0300

    feat(domain): add InvalidJWTError class for handling invalid JWT scenarios

---
+++ packages/domain/src/errors/shared/invalid-jwt.error.ts
@@ -0,0 +1,15 @@
+import { StatusError } from '../../shared/status-error'
+
+export class InvalidJWTError {
+  public readonly errorMessage: string
+  public readonly errorValue: unknown
+  public readonly name: 'InvalidJWTError'
+  public readonly status: StatusError
+
+  constructor(parameters: { message: string; error: unknown }) {
+    this.errorMessage = `Invalid JWT: ${parameters.message}`
+    this.errorValue = parameters.error
+    this.name = 'InvalidJWTError'
+    this.status = StatusError.INVALID
+  }
+}
