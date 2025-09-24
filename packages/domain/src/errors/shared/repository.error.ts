ab67f89e8ef7c8f781e3bdbd68b93b142fd2d857
W I L L <will@gravina.dev>
2025-09-24 09:30:02 -0300

    feat(domain): add RepositoryError class for standardized error handling in repository operations

---
+++ packages/domain/src/errors/shared/repository.error.ts
@@ -0,0 +1,55 @@
+import { StatusError } from '../../shared/status-error'
+
+type ParametersConstructorDTO = {
+  error: unknown
+  repository: {
+    /*
+     * name: RepositoryNames
+     * method: InvestorsRepositoryMethods
+     * externalName: RepositoryExternalName
+     */
+    name: string
+    method: string
+    externalName: string
+  }
+}
+
+/*
+ * export enum RepositoryExternalName {
+ *   PRISMA = 'prisma',
+ *   TYPEORM = 'typeorm'
+ * }
+ */
+
+/*
+ * export enum RepositoryNames {
+ *   INVESTORS = 'investors'
+ * }
+ */
+
+/*
+ * export enum InvestorsRepositoryMethods {
+ *   SAVE = 'save',
+ *   VALIDATE_EMAIL = 'validate email',
+ *   VALIDATE_ID = 'validate id',
+ *   VALIDATE_WHATSAPP = 'validate whatsapp'
+ * }
+ */
+
+export class RepositoryError {
+  readonly status: StatusError
+  readonly errorMessage: string
+  readonly name: 'RepositoryError'
+  readonly errorValue: unknown
+
+  constructor(parameters: ParametersConstructorDTO) {
+    this.errorMessage = `Error in ${parameters.repository.name} repository in ${parameters.repository.method} method.${
+      parameters.repository.externalName === ''
+        ? ''
+        : ` Error in external lib name: ${parameters.repository.externalName}.`
+    }`
+    this.errorValue = parameters.error
+    this.name = 'RepositoryError'
+    this.status = StatusError.REPOSITORY_ERROR
+  }
+}
