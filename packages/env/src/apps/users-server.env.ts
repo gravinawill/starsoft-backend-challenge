8f040bf868d46c23fea9c085098b8960d37039af
W I L L <will@gravina.dev>
2025-09-24 23:35:18 -0300

    feat(env): update users server environment configuration with new validation and additional parameters for Kafka, JWT, and database settings

--- packages/env/src/apps/users-server.env.ts
+++ packages/env/src/apps/users-server.env.ts
@@ -3,9 +3,32 @@ import { z } from 'zod'
 
 export const usersServerENV = createEnv({
   server: {
-    ENVIRONMENT: z.enum(['develop', 'production', 'staging', 'test']),
-    PORT: z.int().positive().optional().default(2222),
-    DATABASE_URL: z.url()
+    USERS_SERVER_ENVIRONMENT: z.enum(['develop', 'production', 'staging', 'test']),
+    USERS_SERVER_PORT: z
+      .string()
+      .transform(Number)
+      .refine((val) => Number.isInteger(val) && val > 0, {
+        message: 'USERS_SERVER_PORT must be a positive integer'
+      })
+      .optional()
+      .transform(Number),
+
+    USERS_SERVER_DATABASE_URL: z.url(),
+
+    USERS_SERVER_KAFKA_BROKER_URL: z.string(),
+
+    USERS_SERVER_JWT_SECRET: z.string(),
+    USERS_SERVER_JWT_EXPIRES_IN_MINUTES: z
+      .string()
+      .transform(Number)
+      .refine((val) => Number.isInteger(val) && val > 0, {
+        message: 'USERS_SERVER_JWT_EXPIRES_IN_MINUTES must be a positive integer'
+      })
+      .optional()
+      .default(14_000)
+      .transform(Number),
+    USERS_SERVER_JWT_ALGORITHM: z.enum(['HS256', 'HS384', 'HS512']),
+    USERS_SERVER_JWT_ISSUER: z.string()
   },
   runtimeEnv: process.env,
   emptyStringAsUndefined: true
