ecd810d1a6f3ddd621d5e7884d8afc4db786c63f
W I L L <will@gravina.dev>
2025-09-28 03:09:49 -0300

    refactor(users-server): streamline error handling in sign-in and sign-up controllers

--- apps/users-server/src/main/controllers/users/sign-in-user.controller.ts
+++ apps/users-server/src/main/controllers/users/sign-in-user.controller.ts
@@ -1,6 +1,7 @@
 import { makeSignInUseCase } from '@factories/use-cases/users/sign-in.use-case.factory'
+import { fastifySendErrorResponse } from '@main/fastify-send-error-response'
 import { type SignInRequest, SignInSuccessResponseSchema } from '@main/route-schemas/users/sign-in.schema'
-import { getHttpStatusCodeByStatusError } from '@niki/domain'
+import { STATUS_ERROR } from '@niki/domain'
 import { HTTP_STATUS_CODE } from '@niki/utils'
 import { type FastifyReply, type FastifyRequest } from 'fastify'
 
@@ -10,30 +11,17 @@ export async function signInUserController(
 ): Promise<void> {
   try {
     const result = await makeSignInUseCase().execute({ credentials: request.body.credentials })
-
-    if (result.isFailure()) {
-      const error = result.value
-      return await reply.status(getHttpStatusCodeByStatusError({ status: error.status }).statusCode).send({
-        success: null,
-        error: {
-          name: error.name,
-          message: error.errorMessage || 'Failed to sign in user'
-        }
-      })
-    }
-
-    return await reply.status(HTTP_STATUS_CODE.OK).send(
-      SignInSuccessResponseSchema.parse({
-        success: { access_token: result.value.accessToken },
-        error: null
-      })
-    )
+    if (result.isFailure()) return await fastifySendErrorResponse({ reply, error: result.value })
+    return await reply
+      .status(HTTP_STATUS_CODE.OK)
+      .send(SignInSuccessResponseSchema.parse({ success: { access_token: result.value.accessToken }, error: null }))
   } catch {
-    return await reply.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({
-      success: null,
+    return await fastifySendErrorResponse({
+      reply,
       error: {
+        status: STATUS_ERROR.INTERNAL_ERROR,
         name: 'InternalServerError',
-        message: 'An unexpected error occurred'
+        errorMessage: 'An unexpected error occurred'
       }
     })
   }
--- apps/users-server/src/main/controllers/users/sign-up-user.controller.ts
+++ apps/users-server/src/main/controllers/users/sign-up-user.controller.ts
@@ -1,9 +1,16 @@
 import { makeSignInUseCase } from '@factories/use-cases/users/sign-in.use-case.factory'
 import { makeSignUpUserUseCase } from '@factories/use-cases/users/sign-up-user-use-case.factory'
+import { fastifySendErrorResponse } from '@main/fastify-send-error-response'
 import { type SignUpRequest, SignUpSuccessResponseSchema } from '@main/route-schemas/users/sign-up.schema'
-import { getHttpStatusCodeByStatusError, UserRole } from '@niki/domain'
-import { usersServerENV } from '@niki/env'
-import { makeUsersProducerEvents } from '@niki/message-broker'
+import {
+  STATUS_ERROR,
+  UserRole,
+  type UsersCustomerCreatedEventPayload,
+  type UsersEmployeeCreatedEventPayload
+} from '@niki/domain'
+import { EventContractType } from '@niki/domain/src/contracts/events'
+import { productInventoryServerENV } from '@niki/env'
+import { ClientID, makeMessageBrokerProvider } from '@niki/message-broker'
 import { HTTP_STATUS_CODE } from '@niki/utils'
 import { type FastifyReply, type FastifyRequest } from 'fastify'
 
@@ -12,74 +19,61 @@ export async function signUpUserController(
   reply: FastifyReply
 ): Promise<void> {
   try {
-    const signUpResult = await makeSignUpUserUseCase().execute({ user: request.body })
-
-    if (signUpResult.isFailure()) {
-      const error = signUpResult.value
-      return await reply.status(getHttpStatusCodeByStatusError({ status: error.status }).statusCode).send({
-        success: null,
-        error: {
-          name: error.name,
-          message: error.errorMessage || 'Failed to create user account'
-        }
-      })
-    }
-    const { userCreated } = signUpResult.value
-
-    const messageBrokerEvents = makeUsersProducerEvents({
-      environments: {
-        BROKERS: [usersServerENV.USERS_SERVER_KAFKA_BROKER_URL],
-        CLIENT_ID: usersServerENV.USERS_SERVER_KAFKA_CLIENT_ID
-      }
+    const messageBrokerProvider = makeMessageBrokerProvider({
+      brokers: [productInventoryServerENV.MESSAGE_BROKER_PROVIDER_BROKER_URL],
+      clientID: ClientID.USERS_SERVER
     })
-
-    if (userCreated.role === UserRole.CUSTOMER) {
-      messageBrokerEvents.publishCustomerCreatedEvent({
-        payload: { email: userCreated.email.value, name: userCreated.name, userID: userCreated.id.toString() }
-      })
-      // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- we need to check if the user is an employee
-    } else if (userCreated.role === UserRole.EMPLOYEE) {
-      messageBrokerEvents.publishEmployeeCreatedEvent({
-        payload: { name: userCreated.name, userID: userCreated.id.toString() }
-      })
-    }
-
-    const resultSignInWithEmailAndPassword = await makeSignInUseCase().execute({ user: { id: userCreated.id } })
-
-    if (resultSignInWithEmailAndPassword.isFailure()) {
-      return await reply
-        .status(getHttpStatusCodeByStatusError({ status: resultSignInWithEmailAndPassword.value.status }).statusCode)
-        .send({
-          success: null,
-          error: {
-            name: resultSignInWithEmailAndPassword.value.name,
-            message: resultSignInWithEmailAndPassword.value.errorMessage || 'Failed to sign in user'
+    const signUpResult = await makeSignUpUserUseCase().execute({ user: request.body })
+    if (signUpResult.isFailure()) return await fastifySendErrorResponse({ reply, error: signUpResult.value })
+    const { userCreated } = signUpResult.value
+    await (userCreated.role === UserRole.CUSTOMER
+      ? messageBrokerProvider.sendMessage<UsersCustomerCreatedEventPayload>({
+          eventContractType: EventContractType.CUSTOMER_CREATED,
+          payload: {
+            userID: userCreated.id.toString(),
+            name: userCreated.name,
+            email: userCreated.email.value,
+            createdAt: userCreated.createdAt,
+            updatedAt: userCreated.updatedAt
           }
         })
+      : messageBrokerProvider.sendMessage<UsersEmployeeCreatedEventPayload>({
+          eventContractType: EventContractType.EMPLOYEE_CREATED,
+          payload: {
+            userID: userCreated.id.toString(),
+            name: userCreated.name,
+            createdAt: userCreated.createdAt,
+            updatedAt: userCreated.updatedAt
+          }
+        }))
+
+    const resultSignInWithEmailAndPassword = await makeSignInUseCase().execute({ user: { id: userCreated.id } })
+    if (resultSignInWithEmailAndPassword.isFailure()) {
+      return await fastifySendErrorResponse({ reply, error: resultSignInWithEmailAndPassword.value })
     }
 
-    const response = SignUpSuccessResponseSchema.safeParse({
+    const response = await SignUpSuccessResponseSchema.safeParseAsync({
       error: null,
       success: { access_token: resultSignInWithEmailAndPassword.value.accessToken }
     })
-
     if (!response.success) {
-      return await reply.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({
-        success: null,
+      return await fastifySendErrorResponse({
+        reply,
         error: {
+          status: STATUS_ERROR.INTERNAL_ERROR,
           name: 'InternalServerError',
-          message: 'An unexpected error occurred'
+          errorMessage: 'An unexpected error occurred'
         }
       })
     }
-
     return await reply.status(HTTP_STATUS_CODE.CREATED).send(response.data)
   } catch {
-    return await reply.status(HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({
-      success: null,
+    return await fastifySendErrorResponse({
+      reply,
       error: {
+        status: STATUS_ERROR.INTERNAL_ERROR,
         name: 'InternalServerError',
-        message: 'An unexpected error occurred'
+        errorMessage: 'An unexpected error occurred'
       }
     })
   }
