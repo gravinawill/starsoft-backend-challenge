279666b8551c259441ac317937046dd6641c3355
W I L L <will@gravina.dev>
2025-09-28 17:41:42 -0300

    feat(workspace): add Elasticsearch sink connector configuration, user creation schema, and PostgreSQL database initialization script

---
+++ infra/docker/postgres/init-databases.sql
@@ -0,0 +1,11 @@
+-- Database initialization script
+-- Create users database
+CREATE DATABASE users;
+
+-- Create products_inventory database
+CREATE DATABASE products_inventory;
+
+-- Optional: Grant permissions to the default postgres user on these databases
+-- (This is usually not needed as the postgres user is the owner by default)
+-- GRANT ALL PRIVILEGES ON DATABASE users TO postgres;
+-- GRANT ALL PRIVILEGES ON DATABASE products_inventory TO postgres;
