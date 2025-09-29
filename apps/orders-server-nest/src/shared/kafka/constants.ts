62dc8729bd6b63230533a2016c8c2a035e2d0e50
W I L L <will@gravina.dev>
2025-09-29 15:33:14 -0300

    chore: remove orders-server-nest application files and configurations

--- apps/orders-server-nest/src/shared/kafka/constants.ts
+++
@@ -1,9 +0,0 @@
-export const KAFKA_TOPICS = {
-  USER_CREATED: 'users.customer.created',
-
-  ORDER_CREATED: 'orders.order.created',
-
-  PRODUCT_CREATED: 'products.created'
-} as const
-
-export type KafkaTopic = (typeof KAFKA_TOPICS)[keyof typeof KAFKA_TOPICS]
--- apps/orders-server-nest/src/shared/request-context/request-context.dto.ts
+++
@@ -1,5 +0,0 @@
-export class RequestContext {
-  public requestID: string | undefined
-  public url: string
-  public ip: string | undefined
-}
